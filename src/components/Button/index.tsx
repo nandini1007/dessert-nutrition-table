a2c5f3c3fd1a00064adf2deb693183224af5adf5
vn50hs1 <nandini.katta@walmart.com>
2021-04-06 19:34:04 -0500

    Initial commit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,195 +1,27 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
-import Table from '../Table';
+import React, { ReactNode } from 'react';
+import PropTypes from 'prop-types';
 import classList from './classList';
-import Button from '../Button';
-import AppContext from '../../context/AppContext';
-import Modal from '../Modal';
-import data from './data';
-import CreateForm from '../CreateForm';
-import { addDessert, deleteDessert } from "../../services/dessert";
+import './index.css';
 
-class App extends Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      sortConfig: null,
-      selectedItems: [],
-      data,
-      selectedItemsMap: {},
-      setSortConfig: this.setSortConfig,
-      toggleSelection: this.toggleSelection,
-      toggleAllSelection: this.toggleAllSelection,
-      isShowModal: false,
-      onCreate: this.handleCreateSubmit
-    };
-  }
+interface ButtonPropTypes {
+  children: ReactNode,
+  type?: string,
+  className?: string,
+  disabled?: boolean,
+  onClick?: any,
+};
 
-  componentDidMount() {
-    let res = []
-    try {
-      res = JSON.parse(localStorage.getItem('data')) || data;
-    } catch(e) {
-      console.log(e)
-    }
-    this.setState({
-      data: res
-    });
-  }
-
-  toggleSelection = (item:any) => {
-    const { selectedItems, selectedItemsMap, data } = this.state;
-    const newSelectedItems = [...selectedItems];
-    const newSelectedItemsMap = {
-      ...selectedItemsMap
-    };
-    const index = selectedItems.indexOf(item.id);
-    if(index !== -1) {
-      newSelectedItems.splice(index, 1);
-      newSelectedItemsMap[item.id] = false;
-    } else {
-      newSelectedItems.push(item.id);
-      newSelectedItemsMap[item.id] = true;
-    }
-    this.setState({
-      selectedItems: newSelectedItems,
-      selectedItemsMap: newSelectedItemsMap,
-      isAllSelected: this.checkAllSelected({ selectedItems: newSelectedItems, data }),
-    });
-  }
-
-  checkAllSelected = ({
-    selectedItems,
-    data
-  }: any) => selectedItems.length === data.length;
-
-  setSortConfig = (sortConfig: any) => {
-    this.setState({
-      sortConfig
-    });
-  }
-
-  handleReset = () => {
-    this.setState({
-      sortConfig: null,
-      selectedItems: [],
-      selectedItemsMap: {},
-      isAllSelected: false,
-    });
-  }
-
-  toggleAllSelection = () => {
-    const { isAllSelected, data } = this.state;
-    const selectedItems:any[] = [];
-    const selectedItemsMap:any = {};
-    if (!isAllSelected) {
-      _.each(data, (item) => {
-        selectedItems.push(item.id);
-        selectedItemsMap[item.id] = true;
-      });
-    }
-    this.setState({
-      selectedItems,
-      selectedItemsMap,
-      isAllSelected: !isAllSelected
-    });
-  }
-
-  handleDelete = () => {
-    deleteDessert()
-    .then(() => {
-      const { selectedItemsMap, data } = this.state;
-      const newData:any[] = [];
-      _.each(data, item => {
-        if (!selectedItemsMap[item.id]) {
-          newData.push(item);
-        }
-      });
-      this.setState({
-        selectedItemsMap: {},
-        selectedItems: [],
-        data: newData,
-        isAllSelected: false,
-      });
-      localStorage.setItem('data', JSON.stringify(newData));
-    });
-  }
-
-  closeForm = () => {
-    this.setState({
-      isShowModal: false
-    });
-  }
-
-  handleCreateSubmit = (reqData) => {
-    addDessert(reqData)
-    .then((res) => {
-      const { data, selectedItems } = this.state;
-      const newData = [...data, res];
-      this.setState({
-        isShowModal: false,
-        data: newData,
-        isAllSelected: this.checkAllSelected({
-          data: newData,
-          selectedItems
-        })
-      });
-      localStorage.setItem('data', JSON.stringify(newData));
-    });
-  }
-
-  openCreateForm = () => {
-    this.setState({
-      isShowModal: true,
-    });
-  }
-
-  render() {
-    const { sortConfig, selectedItems, isShowModal, data } = this.state;
-    return (
-      <AppContext.Provider value={this.state}>
-        <div className={classList.container}>
-            <div className={classList.appTitleBar}>
-              <div className={classList.appTitle}>Nutrition List</div>
-              { !!data.length && <Button onClick={this.handleReset} disabled={!(sortConfig || selectedItems.length !== 0)}>RESET DATA</Button> }
-            </div>
-            {
-              !!data.length && (
-              <div className={classList.featureButtonWrapper}>
-                <div>
-
-                  {
-                    !!selectedItems.length && <span>{selectedItems.length} </span>
-                  }
-                  Selected
-                </div>
-                <div>
-                  <Button onClick={this.openCreateForm}>ADD NEW</Button>
-                  <Button onClick={this.handleDelete} disabled={!selectedItems.length} className="ml3">DELETE</Button>
-                </div>
-              </div>
-              )
-            }
-            {
-              data.length === 0 && (
-                <div className="tc mt">
-                  <p>Please add item in desset list</p>
-                  <Button onClick={this.openCreateForm}>Add New</Button>
-                </div>
-              )
-            }
-            { !!data.length && <Table /> }
-            {
-              isShowModal && (
-                <Modal onClose={this.closeForm}>
-                  <CreateForm />
-                </Modal>
-              )
-            }
-        </div>
-      </AppContext.Provider>
-    );
-  }
+function Button({ type = 'primary', className = '', children, ...props }:ButtonPropTypes ) {
+  const cls = `${classList[type]} ${className}`;
+  return (
+    <button className={cls} {...props}>
+      {children}
+    </button>
+  );
 }
 
-export default App;
+Button.propTypes = {
+  children: PropTypes.any,
+  type: PropTypes.string,
+}
+export default Button;
